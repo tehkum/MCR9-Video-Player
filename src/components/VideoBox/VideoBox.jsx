35a85febe289af1da0bbdb650570a9855dd0f60f
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-07-29 19:46:02 +0530

    watchlater added

--- src/components/VideoBox/VideoBox.jsx
+++ src/components/VideoBox/VideoBox.jsx
@@ -2,22 +2,29 @@ import { useNavigate } from "react-router";
 import "./VideoBox.css";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useVideos } from "../../context/providers/VideoContext";
+import WatchLaterOutlined from "@mui/icons-material/WatchLaterOutlined";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import { useWatchLater } from "../../context/providers/WatchLaterContext";
 
 export default function VideoBox({ props, deleteVid, playlistId }) {
   const navigate = useNavigate();
   const { videoDispatch } = useVideos();
+  const { watchLaterData, removeFromWatchlater, addToWatchlater } =
+    useWatchLater();
 
   return (
     <div
       className="video-box"
-      onClick={() => navigate(`/${props.category}/video/${props?._id}`)}
     >
       <img
         src={props?.thumbnail}
         alt={props?.title}
-        className="vid-box-main-img"
+      onClick={() => navigate(`/${props.category}/video/${props?._id}`)}
+      className="vid-box-main-img"
       />
-      <div className="video-details-layout">
+      <div className="video-details-layout"
+      onClick={() => navigate(`/${props.category}/video/${props?._id}`)}
+      >
         <img src="https://picsum.photos/200/300" alt="..." />
         <div className="video-details">
           <p className="vid-box-heading">
@@ -41,6 +48,15 @@ export default function VideoBox({ props, deleteVid, playlistId }) {
           }
         >
           <DeleteIcon />
+        </div>
+      )}
+      {watchLaterData?.find((item) => item?._id === props?._id) ? (
+        <div className="wl-btn" onClick={() => removeFromWatchlater(props)}>
+          <WatchLaterIcon />
+        </div>
+      ) : (
+        <div className="wl-btn" onClick={() => addToWatchlater(props)}>
+          <WatchLaterOutlined />
         </div>
       )}
     </div>

b3e755912cdbc301e8cca3dee5d7d2848a0da15c
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-07-29 19:48:01 +0530

    link clickable

--- src/components/MoreVideoBox/MoreVideoBox.jsx
+++ src/components/MoreVideoBox/MoreVideoBox.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router";
 import "./MoreVideoBox.css";
 
 export default function MoreVideoBox({ props }) {
+  const navigate = useNavigate();
+
   return (
-    <div className="MoreVideoBox">
+    <div
+      className="MoreVideoBox"
+      onClick={() => navigate(`/${props?.category}/video/${props?._id}`)}
+    >
       <img src={props?.thumbnail} alt={props?.title} />
       <div>
         <p>

9978b68dc0ec3ee2daf81407832eb49ce22f256a
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-07-29 19:38:52 +0530

    search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router-dom";
 
-export default function Header(){
-    return <>
-        <NavLink to="/">Home</NavLink>
+export default function Header() {
+  return (
+    <>
+      {/* <NavLink to="/">Home</NavLink> */}
+      <h2 style={{ textAlign: "center" }}>Video Player</h2>
     </>
+  );
 }
--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,15 +1,27 @@
 import { useVideos } from "../context/providers/VideoContext";
 import "./HomePage.css";
 import VideoBox from "../components/VideoBox/VideoBox";
+import { useState } from "react";
 
 export default function ExplorePage() {
   const { videoState } = useVideos();
+  const [searchFilter, setSearchFilter] = useState("");
+
+  const filterer = videoState?.videoData?.filter((vids) =>
+    vids?.title?.toLowerCase().includes(searchFilter.toLowerCase())
+  );
 
   return (
     <div className="right-side">
       <h2>Explore</h2>
+      <input
+        type="search"
+        placeholder="search"
+        onChange={(e) => setSearchFilter(e.target.value)}
+        style={{padding: "0.5rem", marginBottom: "0.5rem"}}
+      />
       <div className="video-list">
-        {videoState?.videoData?.map((vids) => (
+        {filterer?.map((vids) => (
           <VideoBox props={vids} />
         ))}
       </div>
